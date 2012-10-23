439cb79d92761495b0a763d469ff76e9ba42b6c8
Thomas Lim <thomas.lim@htwg-konstanz.de>
2012-10-23 21:27:32 +0200

    Project initialization.

---
+++ Tetris/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * 	Tetris
+ * 	Softwaretechnik WS 12/13
+ * 
+ * @Author: Thomas Lim
+ * 			Andreas Hummelsberger
+ * 
+ * Main.java
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
