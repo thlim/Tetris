4c08a5b7083d3eb5ea32a25f5c65f495b29d538d
anhummel <anhummel@htwg-konstanz.de>
2013-05-29 16:43:07 +0200

    brick hinzugefügt

--- Tetris/src/model/Brick.java
+++ Tetris/src/model/Brick.java
@@ -1,5 +1,71 @@
 package model;
+import java.math.*;
 
 public class Brick
 {
+	
+	private boolean form [][];
+	private int fieldcount;
+	private int last[];
+	
+	public Brick(){
+		
+		form = new boolean [4][4];
+		fieldcount = 0;
+		last = new int[2];
+		
+		for(int y = 0; y<4; y++){
+			for(int x = 0; x<4; x++){
+				
+				
+				//erstes Element
+				if(fieldcount == 0){
+					form[x][y] = rand();
+					if (form[x][y] == false){
+						continue;	
+					}
+					else fieldcount++;
+					last[0] = x;
+					last[1] = y;
+					continue;
+				}
+				if(fieldcount <= 4){
+					form[x][y] = rand();
+					if(form[x][y] == true){
+						
+						if (last[1] == y && last[0] <= (x - 2)){
+							form[x][y] = false;
+							continue;
+						}
+						else if(last[0] == x && last[1] <= (y - 2)){
+							form[x][y] = false;
+							continue;
+							
+						}
+						else fieldcount++;
+						last[0] = x;
+						last[1] = y;
+						continue;
+					}
+				}
+				else form[x][y] = false;
+				continue;
+				
+				
+			}
+		}
+		
+		
+		
+	}
+	private boolean rand(){
+		int a = (int) (Math.random() * (2 - 1) + 1);
+		if(a == 1){
+			return false;	
+		}
+		return true;
+		
+		
+	}
+	
 }
