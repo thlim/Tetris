24ee146abd1eb7afaf465e018ee728dcfb03f3c8
anhummel <anhummel@htwg-konstanz.de>
2013-05-31 14:34:24 +0200

    methode turn(Brick b) hinzugefuegt

--- Tetris/src/model/Brick.java
+++ Tetris/src/model/Brick.java
@@ -34,45 +34,45 @@ public class Brick
 		switch(scene){
 		
 		case 0:
-			form[0][0] = true;
-			form[1][0] = true;
-			form[0][1] = true;
 			form[1][1] = true;
+			form[2][1] = true;
+			form[1][2] = true;
+			form[2][2] = true;
 			break;
 		
 		case 1:
-			form[0][0] = true;
-			form[0][1] = true;
-			form[0][2] = true;
-			form[0][3] = true;
+			form[1][0] = true;
+			form[1][1] = true;
+			form[1][2] = true;
+			form[1][3] = true;
 			break;
 			
 		case 2:
-			form[0][0] = true;
-			form[0][1] = true;
-			form[0][2] = true;
+			form[1][0] = true;
+			form[1][1] = true;
 			form[1][2] = true;
+			form[2][2] = true;
 			break;
 			
 		case 3:
-			form[0][0] = true;
-			form[1][0] = true;
-			form[2][0] = true;
+			form[0][1] = true;
 			form[1][1] = true;
+			form[2][1] = true;
+			form[1][2] = true;
 			break;
 		
 		case 4:
-			form[0][0] = true;
-			form[1][0] = true;
+			form[0][1] = true;
 			form[1][1] = true;
-			form[2][1] = true;
+			form[1][1] = true;
+			form[2][2] = true;
 			break;
 		
 		case 5:
-			form[1][0] = true;
-			form[2][0] = true;
-			form[0][1] = true;
 			form[1][1] = true;
+			form[2][1] = true;
+			form[0][2] = true;
+			form[1][2] = true;
 			break;
 			
 		case 6:
@@ -83,6 +83,34 @@ public class Brick
 			break;
 		
 		}
+	}
+	
+	public void turn (Brick b){
+		boolean temp[][] = new boolean [4][4];
+		
+		temp[0][0] = b.form[0][3];
+		temp[1][0] = b.form[0][2];
+		temp[2][0] = b.form[0][1];
+		temp[3][0] = b.form[0][0];
+		
+		temp[0][1] = b.form[1][3];
+		temp[1][1] = b.form[1][2];
+		temp[2][1] = b.form[1][1];
+		temp[3][1] = b.form[1][0];
+		
+		temp[0][2] = b.form[2][3];
+		temp[1][2] = b.form[2][2];
+		temp[2][2] = b.form[2][1];
+		temp[3][2] = b.form[2][0];
+		
+		temp[0][3] = b.form[3][3];
+		temp[1][3] = b.form[3][2];
+		temp[2][3] = b.form[3][1];
+		temp[3][3] = b.form[3][0];
+		
+		b.form = temp;
+		
+		
 	}
 	
 	private int rand(){
