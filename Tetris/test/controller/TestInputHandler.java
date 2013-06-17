512feb4499a0dd8eddee74c442078d3109ded702
anhummel <anhummel@htwg-konstanz.de>
2013-06-17 23:06:18 +0200

    TestCases hinzugefuegt

--- Tetris/test/controller/TestInputHandler.java
+++ Tetris/test/controller/TestInputHandler.java
@@ -2,23 +2,43 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import java.awt.Robot;
+import java.awt.event.AWTEventListener;
+import java.awt.event.KeyEvent;
+
+import model.Brick;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestInputHandler {
+	
+	InputHandler ih;
+	Robot robo;
+	Game g;
+	Brick b;
 
 	@Before
 	public void setUp() throws Exception {
+		robo = new Robot();
+		ih = InputHandler.getInstance();
+		b = Brick.getInstance();
+		
 	}
 
 	@Test
 	public void testGetInstance() {
-		assertTrue(true);
+		
+		assertNotNull(ih);
 	}
 
+	
+	
+	
 	@Test
-	public void testKeyPressed() {
+	public void testKeyPressed37() {
 		assertTrue(true);
+		
 	}
 
 	@Test
@@ -30,5 +50,7 @@ public class TestInputHandler {
 	public void testKeyTyped() {
 		assertTrue(true);
 	}
+	
+
 
 }
