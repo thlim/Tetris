2804e3439dccd9b9aff909b3991d95ee72e5ffbb
anhummel <anhummel@htwg-konstanz.de>
2013-06-25 18:19:20 +0200

    TestTui hinzugefuegt

---
+++ Tetris/test/view/TestTUI.java
@@ -0,0 +1,36 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import model.IModel;
+import model.ModelImpl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTUI {
+
+	TUI tui;
+	IModel model;
+	
+	@Before
+	public void setUp() throws Exception {
+		tui = TUI.getInstance();
+		model = new ModelImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		tui = null;
+		model = null;
+	}
+
+	@Test
+	public void testPrintGame() {
+		model.setMapValue(9, 0, true);
+		tui.printGame();
+		assertTrue(true);
+	}
+
+}
