34773facaf0c79a740da0500ecd8f1f702f9deb8
anhummel <anhummel@htwg-konstanz.de>
2013-06-28 02:16:02 +0200

    aufgeräumt

--- Tetris/src/controller/Game.java
+++ Tetris/src/controller/Game.java
@@ -15,6 +15,7 @@ public final class Game {
 	private IView view;
 	private Collision coll;
 	private static Game instance = null;
+	private final int timeMulti = 1000;
 	
 	public static Game getInstance() {
 		if (instance == null) {
@@ -39,7 +40,7 @@ public final class Game {
 
 	private void update(long time) {
 
-		if (time >= 1000 / model.getLevel()) {
+		if (time >= timeMulti / model.getLevel()) {
 
 			if (coll.isCollisionAhead()) {
 				model.addBrick();
@@ -60,7 +61,7 @@ public final class Game {
 		switch (model.getState()) {
 		case 0:
 			update(time);
-			if (time >= 1000 / model.getLevel()) {
+			if (time >= timeMulti / model.getLevel()) {
 				
 				view.update();
 
